3b44b7cc0aacd57db83f2a86b9c4389a7d9bef77
kabathaisaak <kabathaisaak@gmail.com>
2024-07-15 12:14:35 +0300

    this is initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {AppNavbar} from "./navbar/navbar.component";
+import {HeaderComponent} from "./header/header.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AppNavbar,HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular18';
+  title =  'this is loaded dynamically'
+  // : string ='url'
+  isDisabled: boolean = true;
+  isActive: boolean = true;
+  fruitName: string = "Apple"
+
+  buttonclick(){
+    console.log('buttonclicked');
+
+}
 }
